54089100763e82dd0dd87d1fb6a4116dcb6f8195
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-23 11:16:38 -0700

    subclass

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+# from .models import User, UserPremium
+
+class userAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+# # Register your models here.
+admin.site.register(User, userAdmin)
+# admin.site.register(UserPremium)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from uuid import uuid4
+# from django.contrib.auth.models import User
+
 # Create your models here.
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
     post = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
+# class UserPremium(User):
+#     user_prem = models.ForeignKey (User, on_delete=models.CASCADE)
+
